fe84d0413854e98b4c321565ece80ee4f158ceae
Wallace-GS <wgsantos29@gmail.com>
2020-08-15 14:34:23 -0700

    sets up video and face canvas

--- pixelated-face.js
+++ pixelated-face.js
@@ -1,3 +1,33 @@
-// The face detection does not work on all browsers and operating systems.
-// If you are getting a `Face detection service unavailable` error or similar,
-// it's possible that it won't work for you at the moment.
+const video = document.querySelector('.webcam');
+const canvas = document.querySelector('.video');
+const ctx = canvas.getContext('2d');
+ctx.strokeStyle = '#ffc600';
+ctx.lineWidth = 2;
+const faceCanvas = document.querySelector('.face');
+const faceCtx = canvas.getContext('2d');
+
+const faceDetector = new window.FaceDetector();
+
+const populateVideo = async () => {
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: { width: 1280, height: 720 },
+  });
+  video.srcObject = stream;
+  await video.play();
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  faceCanvas.width = video.videoWidth;
+  faceCanvas.height = video.videoHeight;
+};
+
+const detect = async () => {
+  const face = await faceDetector.detect(video);
+  face.forEach(drawFace);
+  requestAnimationFrame(detect);
+};
+
+const drawFace = () => {
+  const { width, height, top, left } = face.boundingBox;
+};
+
+populateVideo().then(detect);
